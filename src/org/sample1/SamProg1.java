36a435eae1d5a33c3e20450c151445c0ddffbfba
nalinianbu33 <nalinianbu33@gmail.com>
2021-10-10 12:37:50 +0530

    Task Added

---
+++ src/org/sample1/SamProg1.java
@@ -0,0 +1,15 @@
+package org.sample1;
+
+public class SamProg1 {
+	public static void username() {
+		System.out.println("UserName");
+	}
+	public static void password() {
+		System.out.println("PassWord");
+	}
+	public static void main(String[] args) {
+		username();
+		password();
+	}
+
+}
